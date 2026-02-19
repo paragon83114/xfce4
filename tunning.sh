433612835705ccce4931cfcfcd26a2e076c2f7a9
paragon83114 <155473681+paragon83114@users.noreply.github.com>
2026-02-19 17:04:53 +0100

    Update tunning.sh

--- tunning.sh
+++ tunning.sh
@@ -4,4 +4,7 @@ for f in $HOME/Desktop/*.desktop; do
     gio set "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"
 done
 
-
+termux-x11-preference "fullscreen":"true"
+termux-x11-preference "showAdditionalKbd":"false"
+termux-x11-preference "displayResolutionMode":"custom"
+termux-x11-preference "displayResolutionExact":"1280x800"
