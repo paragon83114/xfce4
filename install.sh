89bf99e3acd8837ef8147663026c0cf31249ac66
paragon83114 <155473681+paragon83114@users.noreply.github.com>
2026-05-02 10:36:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,8 +37,8 @@ termux-x11-preference "showAdditionalKbd":"false"
 termux-x11-preference "displayResolutionMode":"custom"
 termux-x11-preference "displayResolutionCustom":"1280x800"
 
-echo "alias start=$HOME/xfce4/start.sh" >> $HOME/.bashrc
-echo "alias stop=$HOME/xfce4/stop.sh" >> $HOME/.bashrc
+echo "alias start=$HOME/xfce4/start_adreno_830.sh" >> $HOME/.bashrc
+echo "alias stop=$HOME/xfce4/stop_adreno_830.sh" >> $HOME/.bashrc
 chmod +x $HOME/.bashrc
 
 chmod +x $HOME/xfce4/*.sh
